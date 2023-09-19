f357a353850eb4e5bd094cad38d75c21b4899dee
Farhan Faiyaz <93861628+Superman-0-0-7@users.noreply.github.com>
2023-09-19 14:31:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function redirectTo(option) {
+    if (option === 'todo') {
+      window.location.href = 'todo.html';  // Redirect to your To-Do List page
+    } else if (option === 'bmi') {
+      window.location.href = 'bmi.html';  // Redirect to your BMI Calculator page
+    } else if (option === 'binary') {
+      window.location.href = 'binary.html';  // Redirect to your Binary to Decimal Converter page
+    }
+  }
+  
+
+ 
+
